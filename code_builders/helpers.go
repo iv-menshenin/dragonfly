e030f1ef9fcd27a1e1c2b2800d6a41e46a0f8745
Igor Menshenin <main@devalio.ru>
2020-07-03 13:19:27 +0300

    Added IfKeyVal helper

--- code_builders/helpers.go
+++ code_builders/helpers.go
@@ -7,6 +7,7 @@ func MakeVarNames(vars ...string) VarNames {
 	return vars
 }
 
+// returns an []ast.Expr, any nil values will be excluded from this array
 func E(first ast.Expr, next ...ast.Expr) []ast.Expr {
 	var result = make([]ast.Expr, 0, len(next)+1)
 	if first != nil {
@@ -18,4 +19,15 @@ func E(first ast.Expr, next ...ast.Expr) []ast.Expr {
 		}
 	}
 	return result
+}
+
+// returns ast.KeyValueExpr or nil if the `value` attribute is nil. useful with E helper
+func IfKeyVal(key string, value ast.Expr) ast.Expr {
+	if value == nil {
+		return nil
+	}
+	return &ast.KeyValueExpr{
+		Key:   ast.NewIdent(key),
+		Value: value,
+	}
 }
